af3b1473275f4f09bbed1aa27da07af60bea94d0
andreaspenz <andreas.penz.1989@gmail.com>
2023-08-30 20:48:45 +0200

    add extensions and plugins

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-import { type Linter } from 'eslint'
+import {type Linter} from 'eslint'
 
 const config: Linter.Config = {
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
+    ],
+    plugins: [
+        '@typescript-eslint'
+    ],
     parser: '@typescript-eslint/parser'
 }
 
