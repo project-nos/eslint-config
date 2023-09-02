f752e99f08c6aab813e8114a0460a8eadb35fd49
andreaspenz <andreas.penz.1989@gmail.com>
2023-09-02 09:16:40 +0200

    extend existing configs instead of copying rules

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import { type Linter } from "eslint";
+import globals from "globals";
 
 const config: Linter.Config = {
+  plugins: ["@typescript-eslint", "eslint-comments", "promise", "unicorn"],
   extends: [
-    "eslint:recommended",
+    "airbnb-base",
+    "airbnb-typescript/base",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:eslint-comments/recommended",
+    "plugin:promise/recommended",
+    "plugin:unicorn/recommended",
     "prettier",
-    "./base",
   ],
+  parser: "@typescript-eslint/parser",
+  env: {
+    commonjs: true,
+    es6: false,
+    node: true,
+  },
+  globals: {
+    ...globals.es2020,
+  },
 };
 
 export = config;
